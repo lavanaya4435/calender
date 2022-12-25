d75143df6c4fc5435317c967840be0847f094afe
Lavanya Reddy <113541450+lavanaya4435@users.noreply.github.com>
2022-12-25 13:11:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
-const currentMonth = document.querySelector(".current-month");
-const calendarDays = document.querySelector(".calendar-days");
-const buttons = document.querySelectorAll(".btn");
-let date = new Date();
+var dt=new Date();
+function renderDate(){
+   
+dt.setDate(1);
+var day=dt.getDay();
+// console.log(dt.getDate());
+var endDate=new Date(
+    dt.getFullYear(),dt.getMonth()+1,0
+    ).getDate();
+    // console.log(endDate);
 
-currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
-renderCalendar();
+var prevDate=new Date(
+    dt.getFullYear(),dt.getMonth(),0
+    ).getDate();
+    console.log(prevDate);
 
-function renderCalendar(){
-    const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
-    const totalMonthDay = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
-    const startWeekDay = new Date(date.getFullYear(),date.getMonth(),1).getDay();
+var today=new Date();
+
+var months=["January","Feb","Mar","April","May","June","july","Aug","Sep","Oct","Nov","Dec"];
+document.getElementById("date_str").innerHTML=dt.toDateString();
+document.getElementById("month").innerHTML=months[dt.getMonth()];
+var cells="";
+for(x=day;x>0;x--){
+    cells += "<div class='pre_date'>" + (prevDate - x + 1) + "</div>"
     
-    calendarDays.innerHTML = "";
-
-    const totalCalendarDay = 6 * 7;
-    for (let i = 0; i < totalCalendarDay; i++) {
-        const day = i-startWeekDay;
-
-        if(i <= startWeekDay){
-            calendarDays.innerHTML += `<div class='padding-day'>${prevLastDay-i}</div>`;    // adding previous month days
-        }else if(i <= startWeekDay+totalMonthDay){
-            date.setDate(day);
-            date.setHours(0,0,0,0);
-            const dayClass = date.getTime()===new Date().setHours(0,0,0,0) ? 'current-day' : 'month-day'; 
-            calendarDays.innerHTML += `<div class='${dayClass}'>${day}</div>`;              // adding this month days
-        }else{
-            calendarDays.innerHTML += `<div class='padding-day'>${day-totalMonthDay}</div>`;// adding next month days
-        }
     }
+    // console.log(cells);
+        for(i=1;i<=endDate;i++){
+    if(i==today.getDate() && dt.getMonth()==today.getMonth()){
+        cells+="<div class='today'>"+i+"</div>";
+    }
+    else{
+        cells+="<div>"+i+"</div>";
+
+    }
+   
+}
+document.getElementsByClassName("days")[0].innerHTML=cells;
 }
 
-buttons.forEach((btn) => btn.addEventListener("click", (e) => {
-    const btnClass = String(e.target.classList);
-    date = new Date(currentMonth.textContent);
-    if(btnClass.includes("today")){
-        date = new Date();
-    }else if(btnClass.includes("-year")){
-        date = new Date(date.getFullYear() + (btnClass.includes("prev") ? -1 : 1), 0, 1);
-    }else if(btnClass.includes("-month")){
-        date.setMonth(date.getMonth() + (btnClass.includes("prev") ? -1 : 1));
+function moveDate(para){
+    if(para=='pre'){
+        dt.setMonth(dt.getMonth()-1);
     }
-    currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
-    renderCalendar();
-}));
+    else if(para=='next'){
+        dt.setMonth(dt.getMonth()+1);
+    }
+    renderDate();
+
+}
